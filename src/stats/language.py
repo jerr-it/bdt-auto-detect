da350f6f5eeebd43cbbee24e212ce9180dc842ff
Jerrit Gläsker <jrgls.no-reply@gmx.de>
2023-10-22 17:14:17 +0200

    Added language candidate generation

--- src/stats/language.py
+++ src/stats/language.py
@@ -1,3 +1,4 @@
+import itertools
 from typing import Callable
 
 
@@ -22,6 +23,21 @@ class Language:
         return result
 
 
+# H is the set of possible languages
+H = {
+    str.isupper: ["Lu", "L", "A"],
+    str.islower: ["Ll", "L", "A"],
+    str.isdigit: ["D", "A"],
+    lambda x: True: ["S", "A"],
+}
+
+# L is the set of candidate languages
+L = []
+for candidate in itertools.product(*H.values()):
+    L.append(Language(dict(zip(H.keys(), candidate))))
+
+# G, crude generalization language
+# Not to be confused with the G of the greedy algorithm in the paper
 G = Language({
     str.isdigit: "D",
     str.isupper: "U",
