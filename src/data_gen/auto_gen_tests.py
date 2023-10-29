b705e05055b29412efd3529e1bc07a4b4ff168a2
spiekermax <spiekermann.service@gmail.com>
2023-10-29 16:00:42 +0100

    Minor bugfix, print progress

--- src/data_gen/auto_gen_tests.py
+++ src/data_gen/auto_gen_tests.py
@@ -2,26 +2,32 @@ import glob
 import os
 
 import pandas as pd
+
 from src.data_gen.training_data_generation import TestSet
 
 path = os.path.join(os.path.curdir, "248", "WDC", "scsv", "248")
 
-li = []
+dataframes = []
+filenames = os.listdir(path)
 
-count = 0
-for filename in os.listdir(path):
-
+for index, filename in enumerate(filenames):
     df = pd.read_csv(os.path.join(path, filename), header=0, dtype=object)
 
     # iterate over all columns, and remove column if the first value has more than 15 characters
     for col in df.columns:
+        if df.empty or df[col].empty:
+            continue
+
         if len(str(df[col].iloc[0])) > 15:
             df = df.drop(col, axis=1)
 
     df = df.dropna(axis=1, how="all")
-    li.append(df)
+    dataframes.append(df)
 
-test_set = TestSet(li)
+    print("Reading files... " + "{:.2f}".format(index / len(filenames) * 100) + "%")
+
+print("Generating test set...")
+test_set = TestSet(dataframes)
 gen_test = test_set.generate_test_set(10000)
 
 for t in gen_test:
