4bcdb7c3c7584391d5ff6d4926a3a0be242b50ce
spiekermax <spiekermann.service@gmail.com>
2024-01-14 17:17:41 +0100

    Adjusted tests

--- src/test_auto_detect.py
+++ src/test_auto_detect.py
@@ -2,9 +2,9 @@ import argparse
 import os
 import pprint
 from enum import Enum
-from typing import Callable, Dict, Optional, Tuple
+from typing import Callable, Dict, List, Tuple
 
-import dill
+import numpy
 import pandas as pd
 
 from auto_detect import AutoDetect
@@ -13,102 +13,6 @@ from auto_detect import AutoDetect
 # -------------------------------
 # Models
 # -------------------------------
-
-class TestFile:
-    def __init__(self, file_path: str):
-        self.df = read_tsv_file(file_path)
-        self.name = os.path.basename(file_path)
-
-        self.parse()
-
-    def parse(self):
-        self.drop_ambiguous()
-
-        self.parse_column_values()
-        self.parse_most_incompatible_pairs()
-        self.parse_labels()
-
-    def drop_ambiguous(self):
-        self.df = self.df[self.df.iloc[:, TestColumn.LABEL] != TestLabel.AMBIGUOUS]
-
-    def parse_column_values(self):
-        self.df.iloc[:, TestColumn.COLUMN_VALUES] = self.df.iloc[:, TestColumn.COLUMN_VALUES].apply(
-            lambda x: x.split("___")
-        )
-
-    def parse_most_incompatible_pairs(self):
-        self.df.iloc[:, TestColumn.MOST_INCOMPATIBLE_PAIR] = self.df.iloc[:, TestColumn.MOST_INCOMPATIBLE_PAIR].apply(
-            lambda x: x.split("___")
-        )
-        self.df.iloc[:, TestColumn.MOST_INCOMPATIBLE_PAIR] = self.df.iloc[:, TestColumn.MOST_INCOMPATIBLE_PAIR].apply(
-            lambda x: list_to_tuple(x)
-        )
-
-    def parse_labels(self):
-        self.df.iloc[:, TestColumn.LABEL] = self.df.iloc[:, TestColumn.LABEL].apply(
-            lambda x: test_label_to_bool(x)
-        )
-
-    def test(self, classifier: Callable[[str, str], Tuple[bool, float]]) -> Dict[str, float]:
-        # Basic statistics
-        correct_pairs = 0  # Medium importance since the pairs may vary depending on the training data
-        correct_scores = 0  # Very low importance since the scores are algorithm dependent
-        correct_labels = 0  # Greatest importance since incompatibility should be independent of specifics
-
-        # Advanced statistics
-        score_delta = 0
-
-        # !!! Note that in the test format each row represents a Wikipedia column
-        for _, column_data in self.df.iterrows():
-            test_pair = column_data.iloc[TestColumn.MOST_INCOMPATIBLE_PAIR]
-            test_score = column_data.iloc[TestColumn.COMPATIBILITY_SCORE]
-            test_label = column_data.iloc[TestColumn.LABEL]
-
-            predicted_pair, predicted_score, predicted_label = self.test_column(column_data, classifier)
-
-            # Basic statistics
-            if predicted_pair == test_pair:
-                correct_pairs += 1
-            if predicted_score == test_score:
-                correct_scores += 1
-            if predicted_label == test_label:
-                correct_labels += 1
-
-            # Advanced statistics
-            score_delta += abs(predicted_score - test_score)
-
-        # Basic statistics
-        pairs_precision = correct_pairs / len(self.df)
-        scores_precision = correct_scores / len(self.df)
-        labels_precision = correct_labels / len(self.df)
-
-        return {
-            "Pair precision": pairs_precision,
-            "Score precision": scores_precision,
-            "Score delta": score_delta,
-            "Label precision": labels_precision
-        }
-
-    def test_column(self, column_data: pd.Series, classifier: Callable[[str, str], Tuple[bool, float]]) \
-            -> Tuple[Tuple[str, str], float, bool]:
-        column_values = column_data.iloc[TestColumn.COLUMN_VALUES]
-
-        worst_pair = None
-        worst_score = float("inf")
-        worst_label = None
-        for i in range(len(column_values)):
-            for j in range(i + 1, len(column_values)):
-                value1 = column_values[i]
-                value2 = column_values[j]
-                label, score = classifier(value1, value2)
-
-                if score < worst_score:
-                    worst_pair = (value1, value2)
-                    worst_score = score
-                    worst_label = label
-
-        return worst_pair, worst_score, worst_label
-
 
 class TestLabel(int, Enum):
     COMPATIBLE = 0
@@ -128,12 +32,112 @@ class TestColumn(int, Enum):
     LABEL = 8
 
 
+class TestFile:
+    def __init__(self, file_path: str):
+        self.df = read_tsv_file(file_path)
+        self.name = os.path.basename(file_path)
+
+        self.parse()
+
+    def parse(self):
+        self.fix_malformed_rows()
+        self.drop_ambiguous()
+
+        self.parse_column_values()
+        self.parse_most_incompatible_pairs()
+        self.parse_labels()
+
+        self.drop_trivial()
+
+    def fix_malformed_rows(self):
+        # Identify rows where the label is missing
+        malformed_rows = pd.isna(self.df[TestColumn.LABEL])
+
+        # Shift values to the right for the identified rows
+        self.df.loc[malformed_rows, self.df.columns[TestColumn.LABEL]] = self.df.loc[malformed_rows, self.df.columns[TestColumn.LABEL - 1]].astype(int)
+        self.df.loc[malformed_rows, self.df.columns[TestColumn.COMPATIBILITY_SCORE]] = self.df.loc[malformed_rows, self.df.columns[TestColumn.COMPATIBILITY_SCORE - 1]].astype(float)
+
+        # Insert NaN for the most incompatible pair
+        self.df.loc[malformed_rows, self.df.columns[TestColumn.MOST_INCOMPATIBLE_PAIR]] = numpy.NAN
+
+    def drop_ambiguous(self):
+        self.df = self.df[self.df.iloc[:, TestColumn.LABEL] != TestLabel.AMBIGUOUS]
+
+    def drop_trivial(self):
+        non_trivial_mask = self.df.iloc[:, TestColumn.COLUMN_VALUES].apply(lambda values: len(values) > 1)
+        self.df = self.df[non_trivial_mask]
+
+    def parse_column_values(self):
+        self.df.iloc[:, TestColumn.COLUMN_VALUES] = self.df.iloc[:, TestColumn.COLUMN_VALUES].apply(
+            lambda x: numpy.NAN if pd.isna(x) else x.split("___")
+        )
+
+    def parse_most_incompatible_pairs(self):
+        self.df.iloc[:, TestColumn.MOST_INCOMPATIBLE_PAIR] = self.df.iloc[:, TestColumn.MOST_INCOMPATIBLE_PAIR].apply(
+            lambda x: numpy.NAN if pd.isna(x) else x.split("___")
+        )
+
+    def parse_labels(self):
+        self.df.iloc[:, TestColumn.LABEL] = self.df.iloc[:, TestColumn.LABEL].apply(
+            lambda x: numpy.NAN if pd.isna(x) else test_label_to_bool(x)
+        )
+
+
+class TestSuite:
+    def __init__(self, dfs: List[pd.DataFrame]):
+        # Merge data frames into one
+        self.df = pd.concat(dfs).drop_duplicates(subset=0)
+
+        # Balance label counts
+        label_column = self.df.iloc[:, TestColumn.LABEL]
+        compatible_count = (label_column == True).sum()
+        incompatible_count = (label_column == False).sum()
+
+        limit = min(compatible_count, incompatible_count)
+        self.df = pd.concat([
+            self.df[self.df[TestColumn.LABEL] == True].head(limit),
+            self.df[self.df[TestColumn.LABEL] == False].head(limit)
+        ])
+
+    def test(self, classifier: Callable[[List[str]], List[str]]) -> Dict[str, float]:
+        # Basic statistics
+        correct_labels = 0  # Greatest importance since incompatibility should be independent of specifics
+
+        # !!! Note that in the test format each row represents a Wikipedia column
+        for _, column_data in self.df.iterrows():
+            test_label = column_data.iloc[TestColumn.LABEL]
+            predicted_label = self.test_column(column_data, classifier)
+
+            # Basic statistics
+            if predicted_label == test_label:
+                correct_labels += 1
+
+        # Basic statistics
+        labels_precision = correct_labels / len(self.df)
+
+        return {
+            "Label precision": labels_precision
+        }
+
+    def test_column(self, column_data: pd.Series, classifier: Callable[[str, str], Tuple[bool, float]]) -> bool:
+        column_values = column_data.iloc[TestColumn.COLUMN_VALUES]
+
+        for i in range(len(column_values)):
+            for j in range(i + 1, len(column_values)):
+                value1 = column_values[i]
+                value2 = column_values[j]
+                label = classifier(value1, value2)
+                if not label: return False
+
+        return True
+
+
 # -------------------------------
 # Utility
 # -------------------------------
 
 def read_tsv_file(file_path: str) -> pd.DataFrame:
-    dataframe = pd.read_csv(file_path, header=None, sep='\t', na_values=['\t\t'])
+    dataframe = pd.read_csv(file_path, header=None, sep='\t', na_values=['\t\t'], encoding='unicode_escape')
     return dataframe
 
 
@@ -172,23 +176,21 @@ def test_label_to_bool(value: TestLabel) -> bool:
 
 parser = argparse.ArgumentParser(description="Test Auto-Detect")
 parser.add_argument("--test_path", type=str, help="Path to the folder containing labeled '.tsv'-files")
-parser.add_argument("--session_path", type=str, help="Path to the session file")
 args = parser.parse_args()
 
-autodetect: Optional[AutoDetect] = None
-dill.load_module(args.session_path)
-
-if autodetect is None:
-    raise ValueError("Auto detect could not be loaded properly from session file")
+# TODO: Add AutoDetect
 
 pp = pprint.PrettyPrinter(depth=1)
 
-# TODO: Compare against seeded random classifier
 test_files = read_test_files(args.test_path)
-for test_file in test_files:
-    statistics = test_file.test(autodetect.predict)
-    print(f"=== Testing against {test_file.name} ===")
-    print(f"Statistics:")
-    pp.pprint(statistics)
-    print()
-    print()
+test_dfs = list(map(lambda file: file.df, test_files))
+test_suite = TestSuite(test_dfs)
+
+statistics = test_suite.test(autodetect.predict)
+statistics_decline_all = test_suite.test(lambda _: [None])
+
+print()
+print(f"=== Test results ===")
+print(f"Precision: {statistics['Label precision']}")
+print(f"Baseline precision (allow everything): {1 - statistics_decline_all['Label precision']}")
+print(f"Baseline precision (decline everything): {statistics_decline_all['Label precision']}")
